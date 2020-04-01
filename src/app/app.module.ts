e0b7dd8848b86b6532dda0a72eb8280ba971fbff
samuwrob <samuel.wrob@gmail.com>
2020-04-01 16:34:51 +0200

    minor implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { NavbarreComponent } from './components/navbarre/navbarre.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarreComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarreComponent } from './navbarre.component';
+import { NavbarComponent } from './navbar.component';
 
 describe('NavbarreComponent', () => {
-  let component: NavbarreComponent;
-  let fixture: ComponentFixture<NavbarreComponent>;
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarreComponent ]
+      declarations: [ NavbarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarreComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
