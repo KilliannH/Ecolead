164aa148de9db503e432f688abccf87bc4dd7ff9
samuwrob <samuel.wrob@gmail.com>
2020-04-01 13:52:36 +0200

    add navbarre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from "./material/material.module";
+import { MaterialModule } from './material/material.module';
+import { NavbarreComponent } from './components/navbarre/navbarre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarreComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
