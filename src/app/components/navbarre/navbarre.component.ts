e8c7b640345b7462d59d7ce783397bbdc8f45298
samuwrob <samuel.wrob@gmail.com>
2020-04-01 16:06:30 +0200

    dynamic navbarre

--- src/app/components/navbarre/navbarre.component.ts
+++ src/app/components/navbarre/navbarre.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbarre.component.css']
 })
 export class NavbarreComponent implements OnInit {
+  clickSoundNumber: number;
+  iconsound: string;
 
-  constructor() { }
+  constructor() {
+    this.clickSoundNumber = 0;
+    this.iconsound = 'audiotrack';
+  }
 
   ngOnInit(): void {
   }
 
+
+  clickSound() {
+    if (this.clickSoundNumber === 0 )
+    { this.clickSoundNumber++;
+      this.iconsound = 'music_off';
+    }else{
+      this.clickSoundNumber--;
+      this.iconsound = 'audiotrack';
+
+    }
+
+
+
+
+  }
 }
