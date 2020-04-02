1b9abc4edb48f557dab91e5f7a745fa159c95f6d
samuwrob <samuel.wrob@gmail.com>
2020-04-02 14:54:54 +0200

    add governor component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LandingPageComponent} from './components/landingpage/landingpage.component';
-import {GameComponent} from './game/game.component';
-import {CreditsComponent} from './credits/credits.component';
+import {GameComponent} from './components/game/game.component';
+import {CreditsComponent} from './components/credits/credits.component';
+import {CommanderComponent} from './components/commander/commander.component';
 
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'game', component: GameComponent},
   {path: 'credits', component: CreditsComponent},
+  {path: 'gamegovernor', component: CommanderComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingPageComponent } from './components/landingpage/landingpage.component';
-import { CreditsComponent } from './credits/credits.component';
+import { CreditsComponent } from './components/credits/credits.component';
 import { HeaderComponent } from './components/landingpage/header/header.component';
 import { MainButtonsComponent } from './components/landingpage/mainbuttons/mainbuttons.component';
-import { GameComponent } from './game/game.component';
+import { GameComponent } from './components/game/game.component';
+import { CommanderComponent } from './components/commander/commander.component';
 
 
 
@@ -23,6 +24,7 @@ import { GameComponent } from './game/game.component';
     CreditsComponent,
     GameComponent,
     HeaderComponent,
+    CommanderComponent,
 
   ],
   imports: [
