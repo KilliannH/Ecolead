7c8bcd100f5a92b6f14fdd244062b209dab75cf3
samuwrob <samuel.wrob@gmail.com>
2020-04-02 10:57:57 +0200

    add header again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainbuttonsComponent } from './components/mainbuttons/mainbuttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
+    MainbuttonsComponent,
   ],
   imports: [
     BrowserModule,
