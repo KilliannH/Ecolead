ebd01f0bb3e825cd1ea49ed1bbe1ef1c4ea4f7f8
Killiann Hervagault <killiann.hervagault@wiztivi.com>
2020-04-08 11:15:51 +0200

    new branch, main ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import {GameComponent} from './game/game.component';
+import {CreditsComponent} from './credits/credits.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingComponent},
+  {path: 'game', component: GameComponent},
+  {path: 'credits', component: CreditsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from "./material/material.module";
+import { GameComponent } from './game/game.component';
+import { MapComponent } from './map/map.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { CreditsComponent } from './credits/credits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    MapComponent,
+    NavbarComponent,
+    LandingComponent,
+    CreditsComponent
   ],
   imports: [
     BrowserModule,
